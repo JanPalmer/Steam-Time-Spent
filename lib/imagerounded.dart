5af48901410db0492ef68afef13c633047147cba
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2023-01-16 14:23:19 +0100

    Update imagerounded.dart

--- lib/imagerounded.dart
+++ lib/imagerounded.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class ImageRounded extends StatelessWidget {
   const ImageRounded({
